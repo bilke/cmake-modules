c016af125ce14740a5307b9881a9ba265c60379f
Lars Bilke <lars.bilke@ufz.de>
2012-06-11 11:19:53 +0200

    Added CMake variables value list script.

---
+++ ListAllCMakeVariableValues.cmake
@@ -0,0 +1,15 @@
+# From http://www.kitware.com/blog/home/post/300
+#
+# Usage:
+#
+#  INCLUDE(ListAllCMakeVariabelValues)
+#  list_all_cmake_variable_values()
+
+function(list_all_cmake_variable_values)
+  message(STATUS "")
+  get_cmake_property(vs VARIABLES)
+  foreach(v ${vs})
+    message(STATUS "${v}='${${v}}'")
+  endforeach(v)
+  message(STATUS "")
+endfunction()
