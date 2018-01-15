72b1081914e339350bb529701df84180dddda098
Lars Bilke <lars.bilke@ufz.de>
2018-01-15 13:25:06 +0100

    Copyright year update.

--- FindMKL.cmake
+++ FindMKL.cmake
@@ -8,7 +8,7 @@
 # - MKL_INCLUDE_DIR
 # - MKL_LIBRARIES
 #
-# Copyright (c) 2012-2017, OpenGeoSys Community (http://www.opengeosys.org)
+# Copyright (c) 2012-2018, OpenGeoSys Community (http://www.opengeosys.org)
 # Distributed under a Modified BSD License.
 # See accompanying file LICENSE.txt or
 # http://www.opengeosys.org/project/license
