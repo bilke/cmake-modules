724facccf19f953f2be9abae6b02c9b5863e4a03
FeRD (Frank Dana) <ferdnyc@gmail.com>
2020-02-17 08:26:14 -0500

    Remove manual escaping in gcovr targets

--- CodeCoverage.cmake
+++ CodeCoverage.cmake
@@ -62,6 +62,10 @@
 # 2020-01-19, Bob Apthorpe
 # - Added gfortran support
 #
+# 2020-02-17, FeRD (Frank Dana)
+# - Make all add_custom_target()s VERBATIM to auto-escape wildcard characters
+#   in EXCLUDEs, and remove manual escaping from gcovr targets
+#
 # USAGE:
 #
 # 1. Copy this file into your cmake modules path.
@@ -319,9 +323,8 @@ function(setup_target_for_coverage_gcovr_xml)
     # Combine excludes to several -e arguments
     set(GCOVR_EXCLUDE_ARGS "")
     foreach(EXCLUDE ${GCOVR_EXCLUDES})
-        string(REPLACE "*" "\\*" EXCLUDE_REPLACED ${EXCLUDE})
         list(APPEND GCOVR_EXCLUDE_ARGS "-e")
-        list(APPEND GCOVR_EXCLUDE_ARGS "${EXCLUDE_REPLACED}")
+        list(APPEND GCOVR_EXCLUDE_ARGS "${EXCLUDE}")
     endforeach()
 
     add_custom_target(${Coverage_NAME}
@@ -392,9 +395,8 @@ function(setup_target_for_coverage_gcovr_html)
     # Combine excludes to several -e arguments
     set(GCOVR_EXCLUDE_ARGS "")
     foreach(EXCLUDE ${GCOVR_EXCLUDES})
-        string(REPLACE "*" "\\*" EXCLUDE_REPLACED ${EXCLUDE})
         list(APPEND GCOVR_EXCLUDE_ARGS "-e")
-        list(APPEND GCOVR_EXCLUDE_ARGS "${EXCLUDE_REPLACED}")
+        list(APPEND GCOVR_EXCLUDE_ARGS "${EXCLUDE}")
     endforeach()
 
     add_custom_target(${Coverage_NAME}
